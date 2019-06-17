b93f0023e29a4ccf358c9816b8b04288b00cc1f0
brebz7 <andrei_brebeanu@yahoo.com>
2019-06-17 17:06:09 +0300

    added build folder

---
+++ client/build/precache-manifest.444010bdbfd2196fb71420ee67f2920d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76422dad520dc64a28c9b40a1a2e1502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "43d8a44a7bad4519c72c",
+    "url": "/static/js/main.2e5a0bfe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
