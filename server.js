455f2699a7741180627c00d68664160dcdaa932e
brebz7 <andrei_brebeanu@yahoo.com>
2019-06-17 16:07:22 +0300

    First commit: app gets coords from client

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+})
+
+app.listen(port, () => console.log(`WeatherApp is running at port ${port}`));
