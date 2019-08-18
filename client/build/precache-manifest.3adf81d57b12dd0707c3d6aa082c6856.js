297df48a576d744fb48607b65ab282bba02b75f6
brebz7 <andrei_brebeanu@yahoo.com>
2019-08-18 18:09:03 +0300

    added build for heroku

--- client/build/precache-manifest.3adf81d57b12dd0707c3d6aa082c6856.js
+++ client/build/precache-manifest.3adf81d57b12dd0707c3d6aa082c6856.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "882d89e28f766e3e5c0c359cfc290d1d",
+    "revision": "08015fc2b8b22697d4a6c0a28752e7d4",
     "url": "/index.html"
   },
   {
-    "revision": "adac00f204235f6409cd",
+    "revision": "0291daa5a01a2d7a6d9e",
     "url": "/static/css/main.2e7fa08d.chunk.css"
   },
   {
-    "revision": "9c1858d535bb9453154f",
-    "url": "/static/js/2.ef619704.chunk.js"
+    "revision": "5547d01e6b6c2461b8cc",
+    "url": "/static/js/2.24e82aeb.chunk.js"
   },
   {
-    "revision": "adac00f204235f6409cd",
-    "url": "/static/js/main.5cce2c8b.chunk.js"
+    "revision": "0291daa5a01a2d7a6d9e",
+    "url": "/static/js/main.12f37a1f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
