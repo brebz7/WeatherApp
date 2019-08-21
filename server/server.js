858f09729ad83a46ed729f5900f2c80ba3cb43f3
brebz7 <andrei_brebeanu@yahoo.com>
2019-08-21 19:42:35 +0300

    Removed cors. Wasnt needed

--- server/server.js
+++ server/server.js
@@ -9,11 +9,7 @@ const infoWeather = require('./infoWeather');
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
